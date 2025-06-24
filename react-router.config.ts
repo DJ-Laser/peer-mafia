f8315d935432a34ae5ed28a00ba7ed3faa7aa5ad
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-06-23 21:16:44 -0400

    fix basename which generates incorrect urls in build

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   appDirectory: "src",
-  basename: "/peer-mafia",
+  basename: "/peer-mafia/",
   ssr: false,
 } satisfies Config;
