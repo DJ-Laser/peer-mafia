c21d39a32e08e5fcd379f789bd0c9bdb1577c06f
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2026-06-11 14:21:53 -0400

    update sam roles

--- src/game/roles.ts
+++ src/game/roles.ts
@@ -38,7 +38,7 @@ export const exampleRoleData = {
       borderClass: "border-blue-500/30",
     },
 
-    Cult: {
+    cult: {
       name: "Cult",
       description: "Cult members",
       textClass: "text-green-400",
@@ -74,7 +74,36 @@ export const exampleRoleData = {
       name: "Cult Leader",
       description:
         "Secretly induct players into your cult. If every living player is in your cult, You win.",
-      teamId: "Cult",
+      teamId: "cult",
+    },
+    Ringleader: {
+      name: "Ring leader",
+      description: "Chose a player to join the Mafia team on the first night.",
+      teamId: "mafia",
+    },
+    Venator: {
+      name: "Venator",
+      description:
+        "Hunt the Mafia. Once per Round chose a player to kill.If you die without using this ability you may use it before you die.",
+      teamId: "town",
+    },
+    Doppelganger: {
+      name: "Doppelganger",
+      description:
+        "Chose a player. When that player dies, absorb thier powers.",
+      teamId: "town",
+    },
+    Sleeper_agent: {
+      name: "sleeper_agent",
+      description:
+        "Play as a regular townsperson until you are target by mafia. At which point you will be converted.",
+      teamId: "town",
+    },
+    Doctor: {
+      name: "Doctor",
+      description:
+        "Chose a player to keep safe at night.You may pick any player, any number of times, once per night.",
+      teamId: "town",
     },
   },
   defaultRoles: ["townsperson", "mafia"],
