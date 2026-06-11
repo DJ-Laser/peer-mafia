d61493de7766c0ea44d6eb07bde637426ae4b066
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2026-06-11 10:48:26 -0400

    enable react-router future flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,12 @@ export default {
   appDirectory: "src",
   basename: "/peer-mafia/",
   ssr: false,
+
+  future: {
+    v8_middleware: true,
+    v8_splitRouteModules: "enforce",
+    v8_viteEnvironmentApi: true,
+    v8_passThroughRequests: true,
+    v8_trailingSlashAwareDataRequests: true,
+  },
 } satisfies Config;
